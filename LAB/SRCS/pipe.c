18a5fbe5ecaca36e5d1dd9141fe26f43bfbf92e8
k0xdama <prioux.mateo@outlook.com>
2024-05-14 16:31:43 +0200

    trucs

--- LAB/SRCS/pipe.c
+++ LAB/SRCS/pipe.c
@@ -3,12 +3,17 @@
 int main(int argc, char **argv, char **envp)
 {
     int fd[2];
+	int	infile;
+	int	outfile;
     pid_t	pid;
 	char **cmd;
+	char **cmd1;
 	
 	(void)argc;
 	(void)argv;
-	cmd = ft_split("ping -c 5 google.fr", ' ');
+	cmd = ft_split("cat", ' ');
+	cmd1 = ft_split("wc -l", ' ');
+	infile = open("input")
 	if (pipe(fd) == -1)
 		exit(EXIT_FAILURE);
 	pid = fork();
